a304c0ef130b876d5ddaa5bb7a2ca6d024ee8319
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-05-17 14:49:18 +0300

    added AWS S3 Buckets

--- dental/settings.py
+++ dental/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
+    'storages',
     # 'allauth',
     # 'allauth.account',
     # 'allauth.socialaccount',
@@ -181,6 +182,20 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
 }
+
+# AWS KEYS
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_S3_ADDRESSING_STYLE = 'virtual'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
 
 # Django Authentication
 # AUTHENTICATION_BACKENDS = [
